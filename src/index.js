6890a0dfb78bc41a2019b82c743234877388fa06
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-08-07 18:52:35 +0300

    update

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="PhoneBook">
+        <BrowserRouter basename="phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
